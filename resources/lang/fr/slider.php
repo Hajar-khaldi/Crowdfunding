4e33d00613e49846fcedd73a2bd501d714f846b0
Hajar-Khaldi <hajarkhaldi1998@gmail.com>
2023-07-13 22:58:58 +0100

    commit nb 5

--- resources/lang/fr/slider.php
+++ resources/lang/fr/slider.php
@@ -9,13 +9,13 @@ return [
     |
     */
 
-    'slider_1_title' => 'Little help, for big cause!',
+    'slider_1_title' => 'Petite aide, pour une grande cause!',
     'slider_1_subtitle' => 'Cras justo odio, dapibus ac facilisis in, egestas eget quam.',
 
-    'slider_2_title' => 'Help people near your',
+    'slider_2_title' => 'Aidez les gens près de chez vous',
     'slider_2_subtitle' => 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id.',
 
-    'slider_3_title' => 'Educating the future',
+    'slider_3_title' => 'Eduquer l\'avenir',
     'slider_3_subtitle' => 'With a small contribution you can help educate hundreds of children.',
 
 
